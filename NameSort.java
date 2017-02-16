cdeb5853a52a56fd36d2e4f5873875da6da30c2e
Niall Curtis <niall@pc-78-31.customer.arofanhouse.studenthousingcompany.lan>
2017-02-16 00:09:05 +0000

    Added Validation

--- NameSort.java
+++ NameSort.java
@@ -5,14 +5,38 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
+class nameInput {
+    public static String getString() {
+        Scanner input = new Scanner(System.in);
+        boolean inputError = true; 
+        String initialString = "";
+
+        while (inputError) {
+            String newString = input.nextLine();
+
+            boolean isString = newString.matches("\\d+");
+
+            if (isString == false) {
+                initialString += newString;
+                inputError = false;
+            }
+            else {
+                System.out.println("Invalid Input Entered.");         
+            }
+        }
+        return initialString;
+    }
+}
+
 public class NameSort {
     public static void main(String[] args) {
-    	Scanner input = new Scanner(System.in);
+        nameInput input = new nameInput();
+
     	System.out.println("Please Enter 3 Names:");
 
-    	String name1 = input.nextLine();
-    	String name2 = input.nextLine();
-    	String name3 = input.nextLine();
+    	String name1 = input.getString();
+    	String name2 = input.getString();
+    	String name3 = input.getString();
         // Take 3 inputs as strings.
 
     	System.out.println("");
